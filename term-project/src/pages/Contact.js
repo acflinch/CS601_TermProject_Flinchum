b9a4728b26900cc22fb301ff8d56215c56d0d71b
acflinch <acflinch@bu.edu>
2024-12-04 23:25:17 -0600

    Contact form created

--- term-project/src/pages/Contact.js
+++ term-project/src/pages/Contact.js
@@ -1,5 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/Home.css";
+import git from "../img/git.png";
+import linkedin from "../img/linked.png";
+
 const Contact = () => {
-    return <h1>Contact</h1>
+    return (
+        <>
+            <div className="content-wrapper">
+                <section className="contact">
+                    <form>
+                        <h2>Contact Form</h2>
+                        <div className="input">
+                            <label>Full Name</label>
+                            <input type="text" className="field" placeholder="Enter Your Name" required/>
+                        </div>
+                        <div className="input">
+                            <label>Email Address</label>
+                            <input type="email" className="field" placeholder="Enter Your Email" required/>
+                        </div>
+                        <div className="input">
+                            <label>Message</label>
+                            <textarea className="field-mess" placeholder="Enter Your Message" required></textarea>
+                        </div>
+                        <button type="submit" className="submit-message" onClick={onDownloadClick}>Submit Message</button>
+                    </form>
+                </section>
+            </div>
+        </>
+    )
 };
 
 export default Contact;
