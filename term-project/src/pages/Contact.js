64fb1b03d85a5c0961a476d30e92042fd076ceba
acflinch <acflinch@bu.edu>
2024-12-05 00:30:05 -0600

    contact page has input fields and email my email

--- term-project/src/pages/Contact.js
+++ term-project/src/pages/Contact.js
@@ -1,27 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../css/Home.css";
+import "../css/Contact.css";
 import git from "../img/git.png";
 import linkedin from "../img/linked.png";
 
 const Contact = () => {
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "44213987-43bf-4d87-872a-7e3f0450f064");
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "applicaton/json"
+            },
+            body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+            console.log("Success", res);
+        }
+    };
+
     return (
         <>
             <div className="content-wrapper">
                 <section className="contact">
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <h2>Contact Form</h2>
                         <div className="input">
                             <label>Full Name</label>
-                            <input type="text" className="field" placeholder="Enter Your Name" required/>
+                            <input type="text" className="field" placeholder="Enter Your Name" name="name" required/>
                         </div>
                         <div className="input">
                             <label>Email Address</label>
-                            <input type="email" className="field" placeholder="Enter Your Email" required/>
+                            <input type="email" className="field" placeholder="Enter Your Email" name="email" required/>
                         </div>
                         <div className="input">
                             <label>Message</label>
-                            <textarea className="field-mess" placeholder="Enter Your Message" required></textarea>
+                            <textarea className="field-mess" placeholder="Enter Your Message" name="message" required></textarea>
                         </div>
                         <button type="submit" className="submit-message">Submit Message</button>
                     </form>
