1507a682b40192a832c7a6929cd54306862f40e2
acflinch <acflinch@bu.edu>
2024-12-06 23:06:31 -0600

    inital projects page set up

--- term-project/src/App.js
+++ term-project/src/App.js
@@ -4,6 +4,7 @@ import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
 
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
+              <Route path="projects" element={<Projects />} />
             <Route path="skills" element={<Skills />} />
             <Route path="contact" element={<Contact />} />
             <Route path="*" element={<NoPage />} />
