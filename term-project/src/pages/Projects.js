d1706f5b143cd18b57ab7425cbf82641bf55c166
acflinch <acflinch@bu.edu>
2024-12-07 00:35:56 -0600

    updated dom manipulation

--- term-project/src/pages/Projects.js
+++ term-project/src/pages/Projects.js
@@ -1,13 +1,19 @@
-import React from "react";
-
-import "../components/ProjectsScript";
+import React, { useState } from "react";
 
 import placeholder from "../img/placeholder.jpg";
 import project1 from "../img/project1.png";
 import project2 from "../img/project2.png";
 import project3 from "../img/project3.png";
 
-const Projects = () => {
+function Projects()  {
+    const [projectTitle, setProjectTitle] = useState("");
+    const [projectDetails, setProjectDetails] = useState("");
+
+    const handleClick = (title, details) => {
+        setProjectTitle(title);
+        setProjectDetails(details);
+    };
+
     return (
         <>
             <div className="content-wrapper">
@@ -15,7 +21,12 @@ const Projects = () => {
                     <h3 className="color-project">Latest Projects</h3>
 
                     <div className="column">
-                        <img src={project1} alt="Project1" id="project1" className="project-img"/>
+                        <img src={project1} alt="Project1" className="project-img" onClick={() =>
+                            handleClick("Interactive Web Form with JavaScript",
+                                "While working through this assignment, I had to expand my knowledge of javascript.\n" +
+                                "Specifically when having to validate the input sections, I had to gain an understanding of \n" +
+                                "connecting the given user input with certain outcomes.")}/>
+
                         <img src={project2} alt="Project2" id="project2" className="project-img"/>
                         <img src={project3} alt="Project3" id="project3" className="project-img"/>
                         <img src={placeholder} alt="Project4" id="project4" className="project-img"/>
@@ -25,8 +36,8 @@ const Projects = () => {
 
             <div className="content-wrapper">
                 <div className="details-container">
-                    <h3 className="color-details" id="details-title"></h3>
-                    <p id="details"></p>
+                    <h3 className="color-details">{projectTitle}</h3>
+                    <p>{projectDetails}</p>
                 </div>
             </div>
         </>
